82c74bd4acf38c00f6630356dfecb12ad13f5813
TIRTH BHATT <tirthc58@gmail.com>
2023-12-11 22:01:56 +0530

    Trainning pipelines\

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,7 @@ from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
+# from src.model_trainer import ModelTrainer
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
